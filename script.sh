ac2d3728d5e73f2014bb4a6172f242e23c795890
Aaron Arora <aaronarora@wustl.edu>
2020-04-19 16:17:11 -0500

    attempting to fix script

--- script.sh
+++ script.sh
@@ -3,11 +3,5 @@
 cd src
 cd com
 cd company
-javac -cp ../../ Main.java
-java -cp ../../ com/company/Main
-cd ..
-cd ..
-cd ..
-cd GUI
-javac GUI.java
-java GUI
+javac -cp ../../ GUI.java
+java -cp ../../ com/company/GUI
